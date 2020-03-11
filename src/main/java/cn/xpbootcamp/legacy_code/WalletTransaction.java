666662e5e500a66f8fa941493829cd3f251c2bf8
yaxin.wang <yaxin.wang@rea-group.com>
2020-03-11 21:20:46 +0800

    Extract method for execute long method.

--- src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
+++ src/main/java/cn/xpbootcamp/legacy_code/WalletTransaction.java
@@ -24,33 +24,22 @@ public class WalletTransaction {
     }
 
     public boolean execute(WalletService walletService) throws InvalidTransactionException {
-        if (buyerId == null || (sellerId == null || amount < 0.0)) {
-            throw new InvalidTransactionException("This is an invalid transaction");
-        }
+        validateBasicInfo();
+
         if (status == STATUS.EXECUTED) return true;
         boolean isLocked = false;
         try {
             isLocked = RedisDistributedLock.getSingletonInstance().lock(id);
-
-            // 锁定未成功，返回false
             if (!isLocked) {
                 return false;
             }
-            if (status == STATUS.EXECUTED) return true; // double check
-            long executionInvokedTimestamp = System.currentTimeMillis();
-            // 交易超过20天
-            if (executionInvokedTimestamp - createdTimestamp > 1728000000) {
-                this.status = STATUS.EXPIRED;
-                return false;
-            }
+            if (status == STATUS.EXECUTED) return true;
+
+            if (isExpired()) return false;
+
             String walletTransactionId = walletService.moveMoney(id, buyerId, sellerId, amount);
-            if (walletTransactionId != null) {
-                this.status = STATUS.EXECUTED;
-                return true;
-            } else {
-                this.status = STATUS.FAILED;
-                return false;
-            }
+
+            return finishTransaction(walletTransactionId);
         } finally {
             if (isLocked) {
                 RedisDistributedLock.getSingletonInstance().unlock(id);
@@ -68,5 +57,30 @@ public class WalletTransaction {
             this.id = "t_" + preAssignedId;
         }
         return id;
+    }
+
+    private void validateBasicInfo() throws InvalidTransactionException {
+        if (buyerId == null || sellerId == null || amount < 0.0) {
+            throw new InvalidTransactionException("This is an invalid transaction");
+        }
+    }
+
+    private boolean isExpired() {
+        long executionInvokedTimestamp = System.currentTimeMillis();
+        if (executionInvokedTimestamp - createdTimestamp > 1728000000) {
+            this.status = STATUS.EXPIRED;
+            return true;
+        }
+        return false;
+    }
+
+    private boolean finishTransaction(String walletTransactionId) {
+        if (walletTransactionId != null) {
+            this.status = STATUS.EXECUTED;
+            return true;
+        } else {
+            this.status = STATUS.FAILED;
+            return false;
+        }
     }
 }
